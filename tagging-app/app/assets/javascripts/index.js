428db75d676f7a23bd8ed64e42927254a616bada
Motiejus <motiejus.labzentis@gmail.com>
2019-07-03 04:45:19 +0300

    added html,css,js

--- tagging-app/app/assets/javascripts/index.js
+++ tagging-app/app/assets/javascripts/index.js
@@ -1,21 +1,46 @@
 document.addEventListener("turbolinks:load", function() {
-  const img = document.getElementsByClassName("main-img-container")[0];
-  img.addEventListener("mousemove", function(e) {
-    let div = document.createElement("div");
-    div.style.position = "absolute";
-    div.style.top = e.pageY - 195 + "px"
-    div.style.left = e.pageX - (document.body.clientWidth * 0.1) - 40 + "px";
-    div.style.height = "80px";
-    div.style.width = "80px";
-    div.style.border = "solid black 5px";
-    img.appendChild(div);
-    div.addEventListener("mouseleave", function() {
-      div.parentNode.removeChild(div);
-    })
-  })
-  setInterval(function() {
-    while (img.childNodes.length > 4) {
-      img.removeChild(img.lastChild);
+  const characters = ["Waldo", "Odlaw"];
+  const img = document.getElementsByClassName("img-wrapper")[0];
+  let pointerWrapper = document.createElement("div");
+  pointerWrapper.classList.add("pointer-wrapper");
+  let div = document.createElement("div");
+  pointerWrapper.hidden = true;
+  div.classList.add("on-hover-pointer");
+  pointerWrapper.appendChild(div);
+  img.appendChild(pointerWrapper);
+  img.addEventListener("mousemove", moveMousePointer);
+
+  function moveMousePointer(e) {
+    if (e.pageX >= 157 + 187 && e.pageX <= 978 + 187 && e.pageY >= 60 + 195 && e.pageY <= 581 + 195) {
+      pointerWrapper.hidden = false;
+      pointerWrapper.style.top = e.pageY - 195 + "px";
+      pointerWrapper.style.left = e.pageX - (document.body.clientWidth * 0.1) - 40 + "px";
+      if (pointerWrapper.childNodes.length > 1) {
+        pointerWrapper.removeChild(pointerWrapper.lastChild);
+      }
+    } else {
+      pointerWrapper.hidden = true;
     }
-  },1000);
+  }
+
+  img.addEventListener("click", function() {
+    characters.forEach(function (element, index) {
+      let selectDiv = document.createElement("div");
+      selectDiv.classList.add("pop-up");
+      if (index == 0) {
+        selectDiv.classList.add("first-popup");
+      }
+      selectDiv.innerHTML = element;
+      pointerWrapper.appendChild(selectDiv);
+    })
+    img.removeEventListener("mousemove", moveMousePointer);
+  })
+
+  img.addEventListener("mouseleave", function() {
+    pointerWrapper.hidden = true;
+  })
+
+  pointerWrapper.addEventListener("mouseleave", function() {
+    img.addEventListener("mousemove", moveMousePointer);
+  })
 })
